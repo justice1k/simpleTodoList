de6c4411e5317323ba49a4c85327c8e46c2ee0a6
justice1k <justiceakpalu11@gmail.com>
2024-05-16 16:27:46 +0000

    added local storage

--- app.js
+++ app.js
@@ -4,20 +4,55 @@ const addTask = document.getElementById('add-task');
 const taskContainer = document.getElementById('task-container');
 const inputTask = document.getElementById('input-task');
 
+
+
+    if (localStorage.length > 0) {
+        for (let i = 0; i < localStorage.length; i++){
+
+            taskContainer.innerHTML += `
+                                
+                                <div id="${i}" class="task">
+                                <li><span>${localStorage.getItem(i)}</span></li>
+                                <button class="checkTask"> ✔ </button>
+                                <button class="deleteTask"> 🗑 </button>
+                                </div>
+                                
+            `
+        }  
+        
+        document.addEventListener('click', function (e) {
+            let target = e.target
+            if (target.classList == 'checkTask') {
+                target.parentElement.style.backgroundColor = "rgb(17, 188, 17)";
+                target.style.backgroundColor = "rgb(17, 188, 17)";
+                target.parentElement.querySelector('li').style.textDecoration = "line-through"                
+                
+            } else if (target.classList == 'deleteTask') {
+                localStorage.removeItem(target.parentElement.getAttribute('id'))
+                target.parentElement.remove()
+            }
+
+        })
+
+    }
+
+
 // Events
 
 // addTask Event
 
 addTask.addEventListener('click', function () {
     let task = document.createElement('div');
+    task.setAttribute('id', localStorage.length);
     task.classList.add('task');
 
     li = document.createElement('li');
     span = document.createElement('span');
-    span.innerHTML = `${inputTask.value}`;
+
+    span.innerHTML = inputTask.value;
+    
     li.appendChild(span);
     task.appendChild(li);
-
 
     let checkButton = document.createElement('button');
     checkButton.innerHTML = ' ✔ ';
@@ -34,28 +69,14 @@ addTask.addEventListener('click', function () {
     let spaceReg = /\w+/g;
 
     if (!spaceReg.test(inputTask.value)) {
-        alert('Enter a task wai🙄')
+        alert('Enter a task.')
         inputTask.value = "";
     } else {
+        localStorage.setItem(task.getAttribute("id"), inputTask.value)
         taskContainer.appendChild(task)
         inputTask.value = "";
+
     };
-
-    // Check button Event
-    checkButton.addEventListener('click', function () {
-        checkButton.parentElement.style.backgroundColor = "rgb(17, 188, 17)";
-        checkButton.style.backgroundColor = "rgb(17, 188, 17)";
-        checkButton.parentElement.querySelector('li').style.textDecoration = "line-through"
-    });
-
-    // Delete Button Event
-
-    deleteButton.addEventListener('click', function (e) {
-        let target = e.target;
-
-        // targeting grandparent which is the task container
-        target.parentElement.remove();
-    });
 
 
 })
